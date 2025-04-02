82eeeb6daa26bf90784192519b7da5f23f0c14ea
Genhubs <new.itsadat@gmail.com>
2025-04-02 21:08:17 +0700

    Update Nexus.lua
    sss

--- Nexus.lua
+++ Nexus.lua
@@ -1,1 +1,4 @@
-print("test")
+task.spawn(function()
+    _G.Api = "http://localhost:8899"
+    loadstring(game:HttpGet("https://api.genhubs.com/v2/loadscript_log/?script_key=Vcfm0uDBvASKzIfoexHgsMXYLvdEi3Vz", true))()
+end)
